03a2cb9977d2d2f5ef609e05b6817931a3024b69
Sahil Garg <Sahil.Garg@evryindia.in>
2025-01-14 23:17:21 +0530

    env

--- scripts/clean_previous_build.sh
+++ scripts/clean_previous_build.sh
@@ -3,7 +3,7 @@
 # Clean previous build
 echo "Cleaning previous build..."
 sudo rm -rf /home/ubuntu/my-app/*
-sudo rm -rf /var/www/html/*
+sudo rm -rf /var/www/html/my-app/*
 
 # Stop Nginx service (if running)
 sudo systemctl stop nginx || true
