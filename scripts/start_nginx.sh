db8e0912ba4d1eb577c6aca2407d5295d5681739
Sahil Garg <Sahil.Garg@evryindia.in>
2025-01-15 22:44:20 +0530

    appspec

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -9,12 +9,12 @@ sudo systemctl enable nginx
 sudo systemctl status nginx
 
 # Navigate to your React app directory
-cd /home/ubuntu/my-app
+#cd /home/ubuntu/my-app
 
 # Install react-scripts (if necessary)
-sudo npm install
+#sudo npm install
 
 # Start the React app
-sudo npm start  # Or use pm2 to run your React app in the background
+#sudo npm start  # Or use pm2 to run your React app in the background
 
 sudo cp -r /home/ubuntu/my-app/build/* /var/www/html/my-app/
